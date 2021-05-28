babae757aadb0410c16a9e9c7601c0cf486a2df8
Gio Wan <gio_wan@live.ca>
2021-05-28 12:57:39 -0600

    Add fields to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,48 @@
+import { TextField, Button } from '@material-ui/core';
+import { useState } from 'react';
+
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    // post information
+    // set loading
+    // analyze response
+    // if fail then show error message
+    // if successful then redirect to home
+
+  }
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <>
+      <h1>Login</h1>
+      <TextField
+        required
+        type="text"
+        id="email"
+        label="Email"
+        value={email}
+        variant="outlined"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <TextField
+        required
+        type="password"
+        id="password"
+        label="Password"
+        value={password}
+        variant="outlined"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit" variant="contained" onClick={(e) => onSubmit(e)}>
+        Login
+      </Button>
+    </>
+  );
 }

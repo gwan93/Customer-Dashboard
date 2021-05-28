577a72c18af59542c151aae68f50fceb1644b57c
Gio Wan <gio_wan@live.ca>
2021-05-28 17:59:48 -0600

    Create Customers table component

---
+++ src/components/Customers.js
@@ -0,0 +1,164 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import {Table,TableBody,TableCell, TableContainer, TableFooter, TablePagination, TableRow, Paper, TableHead, IconButton} from '@material-ui/core';
+import {KeyboardArrowLeft, KeyboardArrowRight} from '@material-ui/icons';
+import FirstPageIcon from '@material-ui/icons/FirstPage'
+import LastPageIcon from '@material-ui/icons/LastPage'
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  }
+}));
+
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 500,
+  },
+  tableHead: {
+    fontWeight: 900,
+    width: 160
+  }
+});
+
+export default function Customers(props) {
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const { customers } = props;
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, customers.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <TableContainer component={Paper}>
+      <h2>All Customers</h2>
+      <Table className={classes.table} aria-label="custom pagination table">
+        <TableHead>
+          <TableCell className={classes.tableHead} align="left">First Name</TableCell>
+          <TableCell className={classes.tableHead} align="left">Last Name</TableCell>
+          <TableCell className={classes.tableHead} align="left">UID</TableCell>
+          <TableCell className={classes.tableHead} align="left">Profession</TableCell>
+          <TableCell className={classes.tableHead} align="left">Date Created</TableCell>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? customers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : customers
+          ).map((row) => (
+            <TableRow key={row.uid}>
+              <TableCell component="th" scope="row">
+                {row.first_name}
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.last_name}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="left">
+                {row.uid}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="left">
+                {row.profession}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="left">
+                {row.date_created}
+              </TableCell>
+            </TableRow>
+          ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={customers.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
+}

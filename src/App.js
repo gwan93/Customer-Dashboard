867f33400e42ed850dc79c758d9a891ac15733f0
Gio Wan <gio_wan@live.ca>
2021-05-29 15:35:27 -0600

    Add cookies

--- src/App.js
+++ src/App.js
@@ -4,8 +4,25 @@ import Dashboard from './components/Dashboard';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import './App.css';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [state, setState] = useState({
+    username: "",
+    userId: null
+  });
+
+  useEffect(() => {
+    if (Cookies) {
+      setState({
+        ...state,
+        username: Cookies.get('username'),
+        userId: Cookies.get('userId')
+      })
+    }
+  }, [state])
+
   return (
     <main>
       <BrowserRouter>

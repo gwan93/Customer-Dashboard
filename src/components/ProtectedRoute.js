45931c9536dc96416dc29557f734ace27de5f271
Gio Wan <gio_wan@live.ca>
2021-05-31 15:34:26 -0600

    Fix bug when refreshing at /dashboard

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,22 +1,36 @@
 import { Route, Redirect } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 export default function ProtectedRoute({ component: Component, state, ...rest }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (state.username) {
+      setLoading(false);
+    }
+  }, [state])
+  
   return (
-    <Route {...rest} render={
-      props => {
-        if (state.username !== "" && state.userId !== null) {
-          return <Component {...rest} {...props} state={state}/>
-        } else {
-          return <Redirect to={
-            {
-              pathname: '/unauthorized',
-              state: {
-                from: props.location
-              }
+    <div>
+      {loading && <h1>Loading data. Please wait.</h1>}
+      {!loading && (
+        <Route {...rest} render={
+          props => {
+            if (state.username !== "" && state.userId !== null) {
+              return <Component {...rest} {...props} state={state}/>
+            } else {
+              return <Redirect to={
+                {
+                  pathname: '/unauthorized',
+                  state: {
+                    from: props.location
+                  }
+                }
+              } />
             }
-          } />
-        }
-      }
-    } />
+          }
+        } />
+      )}
+    </div>
   )
 }

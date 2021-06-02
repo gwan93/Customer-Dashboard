03f356ed9df4340e209f064f409623dacfba17e1
Gio Wan <gio_wan@live.ca>
2021-06-02 15:11:04 -0600

    Remove unnecessary code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core";
-import reportWebVitals from './reportWebVitals';
 
 const theme = createMuiTheme({
   palette: {
@@ -24,8 +23,3 @@ ReactDOM.render(
   </ThemeProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

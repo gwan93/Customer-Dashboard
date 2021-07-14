2ae617319b230d651a2e7d0e59b56aeebf1cc2d2
Gio W <gio_wan@live.ca>
2021-07-14 10:06:56 -0600

    Fix syntax

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+import { createTheme, ThemeProvider } from "@material-ui/core";
 
-const theme = createMuiTheme({
+const theme = createTheme({
   palette: {
     primary: {
       main: "#3580a9",
